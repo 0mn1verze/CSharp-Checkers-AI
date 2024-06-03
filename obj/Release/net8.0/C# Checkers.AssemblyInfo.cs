e52ad9838b2ed772e2010f786c4380c213c01185
0mn1verze <evanfung05@gmail.com>
2024-06-03 13:15:59 +0100

    build release

--- obj/Release/net8.0/C# Checkers.AssemblyInfo.cs
+++ obj/Release/net8.0/C# Checkers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Checkers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7c45ae4c14df5bc73a8df81354d6043e4eb75c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b12acae0c42e17b58476be29493c5fa7e98d5ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Checkers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Checkers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
